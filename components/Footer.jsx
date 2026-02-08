a9affb4e5910e4330284724e880bb6c62d926557
Dev Sharma <devsharma2057@gmail.com>
2026-02-08 16:38:56 +0530

    Update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="w-full h-[60vh] p-2">
-      <section className="bg-black rounded-xl w-full h-full  flex flex-col items-center justify-center">
+    <footer className="w-full min-h-screen relative ">
+      <section className="h-[60vh] bg-black rounded-xl w-full flex flex-col items-center justify-center">
         <div className="flex items-center justify-center flex-col">
           <h1 className="text-accent text-5xl font-bold uppercase">
             Your Brand Deserves Better.
